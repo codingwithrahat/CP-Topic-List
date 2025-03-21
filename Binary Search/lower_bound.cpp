bc00a96d8771a2946fcfcd6ae3dc544e881c2d85
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-03-22 02:05:39 +0600

    Create lower_bound.cpp

---
+++ Binary Search/lower_bound.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int n; cin>>n;
+	
+	int a[n];
+    
+    for(int i = 0; i<n; i++) cin>>a[i];
+    	
+	int l = 0, r = n - 1;
+	
+	int key; cin>>key;
+	int ans = n;
+	
+    while(l <= r){
+        
+        int mid = l + (r - l) / 2;
+        
+        if(key <= a[mid]){
+            ans = mid;
+            r = mid - 1;
+        }else{
+            l = mid + 1;
+        }
+    }
+    
+    cout<<ans<<endl;
+
+}
