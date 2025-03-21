44ff2ba16e1b7ecb9752258cb39d0451a0560a6f
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-03-22 00:48:32 +0600

    Create build_function.cpp

---
+++ Binary Search/build_function.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    vector<int>v = {1, 2, 3, 4, 5};
+    
+    cout<< binary_search(v.begin(), v.end(), 1) << endl;
+    
+    
+}
