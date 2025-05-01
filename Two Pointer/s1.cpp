fe105884d8086c9706b018e2323871f6ec69bf4e
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-02 01:28:10 +0600

    Create s1.cpp

---
+++ Two Pointer/s1.cpp
@@ -0,0 +1,35 @@
+//Find the pair whose sum is equal to x
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool solve(vector<int>&v, int x, int n){ //tc - O(n)
+    int i = 0;
+    int j = n - 1;
+
+    while(i < j){
+        int sum = v[i] + v[j];
+        if(sum == x) return true;
+        else if(sum > x) j--;
+        else i++;
+    }
+    return false;
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int>v(n);
+
+    for(int i = 0; i<n; i++) cin>>v[i];
+
+    sort(v.begin(), v.end()); //tc - O(nlogn)
+
+    int x; cin>>x;
+
+    bool ans = solve(v, x, n);
+
+    if(ans) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+//total tc - O(nlogn)
