2140ff725a01a6bffa8f9c3404dae54b0825dc68
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-01 17:12:33 +0600

    Create update.cpp

---
+++ Segment Tree/update.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+int a[N];
+int tree[N * 4];
+
+
+void init(int node, int s, int e){
+    if(s == e){
+        tree[node] = a[s];
+        return;
+    }
+
+    int left = node * 2;
+    int right = node * 2 + 1;
+
+    int mid = (s + e) / 2;
+
+    init(left, s, mid);
+    init(right, mid + 1, e);
+
+    tree[node] = tree[left] + tree[right];
+
+
+
+}
+
+int query(int node, int s, int e, int i, int j){
+
+
+
+    if(e < i || s > j) return 0;
+    if(s >= i && e <= j) return tree[node];
+
+    int left = node * 2;
+    int right = node * 2 + 1;
+
+    int mid = (s + e) / 2;
+
+    int s1 = query(left, s, mid, i, j);
+    int s2 = query(right, mid + 1, e, i, j);
+
+    return s1 + s2; 
+
+
+}
+
+void update(int node, int s, int e, int i, int value){
+    if(e < i || s > i){
+        return;
+    }
+    if(s >= i && e <= i){
+        tree[node] = value;
+        return;
+    }
+
+
+    int left = node * 2;
+    int right = node * 2 + 1;
+
+    int mid = (s + e) / 2;
+
+    update(left, s, mid, i, value);
+    update(right, mid + 1, e, i, value);
+    tree[node] = tree[left] + tree[right];
+
+
+}
+
+int main(){
+
+    int n; cin>>n;
+
+    for(int i = 1; i<=n; i++){
+        cin>>a[i];
+    }
+
+    init(1, 1, n);
+
+    //a and b , query range
+    int a, b; cin>>a>>b;
+
+    int s = query(1, 1, n, a, b);
+
+
+    cout<<s<<endl;
+
+    //j index , k value
+    int j, k; cin>>j>>k; 
+
+    update(1, 1, n, j, k); 
+
+    int s2 = query(1, 1, n, a, b);
+
+    //after update
+    cout<<s2<<endl;
+
+}
