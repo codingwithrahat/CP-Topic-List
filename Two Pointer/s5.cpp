7f85d923d12258873f02b8894ee06df89bc0b95e
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-03 17:56:59 +0600

    Create s5.cpp

---
+++ Two Pointer/s5.cpp
@@ -0,0 +1,37 @@
+//Find a triplet that sum to a given value
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int a[],int n, int x){
+   
+    for(int i = 0; i<n; i++){
+        int l = i + 1;
+        int r = n - 1;
+
+        while(l < r){
+            
+            int sum = a[i] + a[l] + a[r];
+
+            if(sum == x){
+                cout<<a[i]<<" "<<a[l]<<" "<<a[r]<<endl;
+            }
+            if(sum > x) r--;
+            else l++;
+                
+        
+        }
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    int x; cin>>x;
+
+    int a[n];
+    for(int i = 0; i<n; i++) cin>>a[i];
+
+    sort(a, a + n);
+
+    solve(a, n, x);
+}
