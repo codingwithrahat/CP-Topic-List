a9ec5d9d76abca47bfebd3fdb86486614d8b5f6f
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-11 01:15:23 +0600

    Create getPrimes.cpp

---
+++ Number Theory/getPrimes.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+	if(n < 2) return false;
+
+	for(int i = 2; i * i <= n; i++){
+		if(n % i == 0) return false;
+	}
+	return true;
+}
+
+vector<int> getPrimes(int n){
+
+	vector<int>primes;
+	for(int i = 2; i <= n ; i++){
+		if(isPrime(i)){
+			primes.push_back(i);
+		}
+		
+	}
+
+	return primes;
+}
+
+int main(){
+	int n; cin>>n;
+	
+	vector<int>v = getPrimes(n);
+
+	for(auto u : v) cout<<u<<" ";
+	cout<<endl;
+	
+}
