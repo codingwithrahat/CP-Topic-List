5181f85faa123e6bc3f9ba77d083751251ad5d82
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-11 01:03:18 +0600

    Create isPrime3.cpp

---
+++ Number Theory/isPrime3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+	if(n < 2) return false;
+
+	for(int i = 2; i * i <= n; i++){
+		if(n % 2 == 0) return false;
+	}
+	return true;
+}
+
+int main(){
+	int n; cin>>n;
+	cout<<isPrime(n)<<endl;
+	
+}
