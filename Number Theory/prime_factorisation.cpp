de6244b06d376a383f95927b6e6792beba9c3ba8
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-05-15 17:29:25 +0600

    Create prime_factorisation.cpp

---
+++ Number Theory/prime_factorisation.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n; cin>>n;
+	vector<int>primes_fact;
+
+	for(int i = 2; i<=n; i++){
+		while(n % i == 0){
+			primes_fact.push_back(i);
+			n = n / i;
+		}
+	}
+
+	for(auto &u : primes_fact) cout<<u<<" ";
+	cout<<endl;
+
+	//tc - O(N)
+
+}
