ec96c4d1419ae40050d1784281d3bb5bbfd53575
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-07-31 13:27:09 +0600

    Update build_upper.cpp

--- Binary Search/build_upper.cpp
+++ Binary Search/build_upper.cpp
@@ -1,18 +1,24 @@
+// smallest index such that arr[idx] > key
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	
-	int n; cin>>n;
-	
-	vector<int>v(n);
+    
+    int n; cin>>n;
+    
+    vector<int>v(n);
     
     for(int i = 0; i<n; i++) cin>>v[i];
+
+    sort(v.begin(), v.end());
     
     int key; cin>>key;
-    	
-	int up = upper_bound(v.begin(), v.end(), key) - v.begin();
-	
-	cout<<up<<endl;
+        
+    int up = upper_bound(v.begin(), v.end(), key) - v.begin();
+
+    //give index in zero based indexing
+    
+    cout<<up<<endl;
 
 } 
