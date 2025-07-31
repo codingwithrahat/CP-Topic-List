d2a428ce6eb2031adbdda7c7ee4308f5f8d15049
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-07-31 13:24:09 +0600

    Update build_lower.cpp

--- Binary Search/build_lower.cpp
+++ Binary Search/build_lower.cpp
@@ -1,18 +1,27 @@
+//smallest index such that arr[idx] >= n
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	
-	int n; cin>>n;
-	
-	vector<int>v(n);
+    
+int n; cin>>n;
+    
+    vector<int>v(n);
     
     for(int i = 0; i<n; i++) cin>>v[i];
+
+    sort(v.begin(), v.end());
+
+    //vcetor must be sorted    
     
     int key; cin>>key;
-    	
-	int lo = lower_bound(v.begin(), v.end(), key) - v.begin();
-	
-	cout<<lo<<endl;
+        
+    int lo = lower_bound(v.begin(), v.end(), key) - v.begin();
+
+    //lower_bound() return an iterator point
+    //substract v.begin() from this iterator gives the index
+    
+    cout<<lo<<endl;
 
 } 
