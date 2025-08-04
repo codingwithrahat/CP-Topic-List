a88a1e9fdd0c39e395c637374ab3c266e02795e3
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-04 22:58:54 +0600

    Update interger_bisection.cpp

--- Binary Search/interger_bisection.cpp
+++ Binary Search/interger_bisection.cpp
@@ -1,6 +1,8 @@
 // problem link 
 // https://www.spoj.com/problems/MATHLOVE/
 
+//Way 01
+
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -33,4 +35,66 @@ int main() {
         if (ans == -1) cout << "NAI\n";
         else cout << ans << endl;
     }
+}
+
+
+//Way 02
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define druto() ios::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+#define ff first
+#define ss second
+#define int long long
+#define double long double
+#define pb push_back
+#define ll long long
+
+const int mod = 1e9 + 7;
+
+
+int get(int n){
+  return (n * (n + 1)) / 2;
+}
+
+int32_t main(){
+
+    druto();
+    
+    int N = 1e5 + 5;
+    
+    vector<int>v(N);
+    
+    for(int i = 1; i<N; i++){
+      v[i] = get(i);
+    }
+
+    int t; cin>>t;
+    while(t--){
+        int y; cin>>y;
+        
+        int l = 0, r = N - 1, ans = -1;
+        
+        while(l <= r){
+          int mid = l + (r - l) / 2;
+          
+          if(v[mid] == y){
+            ans = mid;
+            break;
+          }else if(v[mid] < y){
+            l = mid + 1;
+          }else{
+            r = mid - 1;
+          }
+        }
+        
+        if(ans != -1) cout<<ans<<endl;
+        else cout<<"NAI\n";
+     
+        
+    }
+    
+    
 }
