0543d3d51343c63137f9f79560b4437d7a39d867
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-04 23:02:44 +0600

    Update bs_implement.cpp

--- Binary Search/bs_implement.cpp
+++ Binary Search/bs_implement.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-	
-	int n; cin>>n;
-	
-	int a[n];
+    
+    int n; cin>>n;
+    
+    int a[n];
     
     for(int i = 0; i<n; i++) cin>>a[i];
-    	
-	int l = 0, r = n - 1;
-	
-	int key; cin>>key;
-	bool flag = false;
-	
+        
+    int l = 0, r = n - 1;
+    
+    int key; cin>>key;
+    bool flag = false;
+    
     while(l <= r){
         
         int mid = l + (r - l) / 2;
