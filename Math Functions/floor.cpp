86f93e7d857908c6d6329d14ff1a472cc98911d4
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-05 11:49:26 +0600

    Create floor.cpp

---
+++ Math Functions/floor.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 5, b = 4;
+
+    //cout<<(a + b) / 2<<endl; //output - 4
+
+    int ans = floor((a + b) / (2 * 1.0));
+
+    cout<<ans<<endl; //output - 4 , same as normal int division
+    //floor() rounds down a value
+
+
+}
