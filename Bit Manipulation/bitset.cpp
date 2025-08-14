6fa5e6bfce50b2e5863c3d3a1651e49868e97451
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-08-15 00:03:25 +0600

    Create bitset.cpp

---
+++ Bit Manipulation/bitset.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int n = 10; 
+
+   bitset<32>bits(n);
+   //binary of 10 - 1010
+   //32 - bit binary representation
+
+   cout<<bits<<endl;
+
+}
