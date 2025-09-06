3106a304fc1c6892b721f37457224c30bd9e1fc4
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-09-06 23:53:16 +0600

    Update manual_ceil.cpp

--- Math Functions/manual_ceil.cpp
+++ Math Functions/manual_ceil.cpp
@@ -2,24 +2,14 @@
 using namespace std;
 
 int manual_ceil(int a, int b){
-    return (a + b + 1) / 2;
-    //5 + 4 + 1 = 10
-    //10 / 2 = 5
-    //correct cz ceil((5 + 4) / (2 * 1.0))= 5
-
-    //but if a = 5, b = 5 
-    //5 + 5 + 1 = 11
-    //11 / 2 = 5 
-    //which is still correct cz ceil((5 + 5) / (2 * 1.0)) = 5
+    return (a + b - 1) / b;
 }
 
 int main(){
     int a = 5, b = 4;
 
-    //cout<<(a + b) / 2<<endl; output - 4
-
     int ans = manual_ceil(a, b);
 
-    cout<<ans<<endl; // output - 5
+    cout<<ans<<endl;
 
 }
