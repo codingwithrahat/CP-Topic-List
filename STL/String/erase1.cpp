8443c24da867ba5c2aa52902802afcdc371caf5d
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-10-31 22:17:22 +0600

    Create erase1.cpp

---
+++ STL/String/erase1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   string s = "RahatHossan";
+
+   s.erase(2,2);  // delete 2 characters starting from index 2
+   
+   cout<<s<<endl;
+
+   //output - RatHossan 
+
+}
