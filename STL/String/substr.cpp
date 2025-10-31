6c62029b71ef4117ecc61784f49ec5ab84280be7
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-10-31 22:46:43 +0600

    Add comment about time complexity in substr.cpp

---
+++ STL/String/substr.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   string s = "RahatHossan";
+
+   string s1 = s.substr(0,5);
+   string s2 = s.substr(5,6);
+
+   // sunstr(pos, length);
+
+   cout<<s1<<" "<<s2<<endl;
+
+  //TC - O(n)
+
+}
