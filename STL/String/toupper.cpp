2f8229148f269dc5462da81590d9a313572ec413
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-03 00:23:28 +0600

    Create toupper.cpp

---
+++ STL/String/toupper.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   char c = 'a';
+
+   char uc = toupper(c);
+
+   cout<<uc<<endl;
+
+   //converts lowercase to uppercase letter
+   
+
+}
