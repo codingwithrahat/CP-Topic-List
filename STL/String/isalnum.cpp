a31318c44d8ef13217ba98c2f01300197b827219
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-03 00:20:07 +0600

    Create isalnum.cpp

---
+++ STL/String/isalnum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   char c = 'A';
+
+   if(isalnum(c)){
+      cout<<"Alphanumeric\n";
+   }else{
+      cout<<"Not Alphanumeric\n";
+   }
+
+   //isalnum chcek if a char is alphanumeric or not
+   //A-Z or a-z or 0-9 all are alphanumeric
+
+}
