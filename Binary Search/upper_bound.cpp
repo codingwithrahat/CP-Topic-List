635376968c5bf1aa5316c3cd1d99a943de269b23
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-02 23:57:01 +0600

    Update upper_bound.cpp

--- Binary Search/upper_bound.cpp
+++ Binary Search/upper_bound.cpp
@@ -18,11 +18,11 @@ int main() {
         
         int mid = l + (r - l) / 2;
         
-        if(key >= a[mid]){
-            l = mid + 1;
-        }else{
+        if(a[mid] > key){
             ans = mid;
             r = mid - 1;
+        }else{
+            l = mid + 1;
         }
     }
     
