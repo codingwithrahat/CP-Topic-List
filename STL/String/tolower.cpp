a27c3f19b9257bbf23ad93dd07e442addab84ca4
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-03 00:22:31 +0600

    Create tolower.cpp

---
+++ STL/String/tolower.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   char c = 'A';
+
+   char lc = tolower(c);
+
+   cout<<lc<<endl;
+
+   //converts uppercase to lowercase letter
+   
+
+}
