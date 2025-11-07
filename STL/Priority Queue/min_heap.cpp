84e1e61e7fdd4be2ff9708751522b19263c2a9c7
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-07 15:42:59 +0600

    Create min_heap.cpp

---
+++ STL/Priority Queue/min_heap.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   priority_queue<int, vector<int>, greater<int>>pq; //min heap
+
+   pq.push(5);
+   pq.push(3);
+   pq.push(9);
+   pq.push(2);
+
+   while(!pq.empty()){
+      cout<<pq.top()<<endl;
+      pq.pop();
+   }
+
+
+}
