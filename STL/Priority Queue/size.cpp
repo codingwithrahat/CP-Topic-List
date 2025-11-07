e820b0b973d38d738f4291f1d00a54c9bbe1c449
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-07 15:44:14 +0600

    Create size.cpp

---
+++ STL/Priority Queue/size.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   priority_queue<int>pq; 
+
+   pq.push(5);
+   pq.push(3);
+   pq.push(9);
+   pq.push(2);
+
+   cout<<pq.size()<<endl; //print 4
+
+
+}
