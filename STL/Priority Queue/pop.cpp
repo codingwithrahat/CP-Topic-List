bc2a6d65c307af4455b594de55145eb55811b5f0
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-07 15:46:17 +0600

    Create pop.cpp

---
+++ STL/Priority Queue/pop.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   priority_queue<int>pq; 
+
+   pq.push(5);
+   pq.push(3);
+   pq.push(9);
+   pq.push(2);
+
+   pq.pop(); //remove the top element (largest)
+
+   cout<<pq.size()<<endl; //print 3
+
+
+}
