9c72c2eac0196f3ea32a182fc246359b2160fe7b
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-07 15:58:57 +0600

    Create push.cpp

---
+++ STL/Queue/push.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   queue<int>q; //FIFO - First IN First Out (Ticket counter line)
+
+   pq.push(5);
+   pq.push(3);
+   pq.push(9);
+   pq.push(2); //TC - O(1)
+
+   //queue internally look like - 5 , 3, 9, 2
+
+}
