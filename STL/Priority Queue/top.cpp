42860c2e25c15e071be4a8d6009c147c1b8641c4
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-07 15:49:13 +0600

    Create top.cpp

---
+++ STL/Priority Queue/top.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   priority_queue<int>pq; 
+
+   pq.push(5);
+   pq.push(3);
+   pq.push(9);
+   pq.push(2); 
+
+
+   cout<<pq.top()<<endl; //print the top element (largest), TC- O(1)
+
+
+}
