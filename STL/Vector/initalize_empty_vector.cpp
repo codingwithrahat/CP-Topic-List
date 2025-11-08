efa83fd12db27bbf1f8f82b71904108bda0950fe
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-08 18:25:46 +0600

    Create initalize_empty_vector.cpp

---
+++ STL/Vector/initalize_empty_vector.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   vector<int>v;
+
+   v.push_back(1); //TC - O(1)
+   v.push_back(2);
+
+   //initialize empty vector, we can push_back elements later
+   
+
+}
