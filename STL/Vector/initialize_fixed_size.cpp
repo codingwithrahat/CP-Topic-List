58ef5b72b5040d8ad0222b9a36820fc4a974541e
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-08 18:34:18 +0600

    Update initialize_fixed_size.cpp

--- STL/Vector/initialize_fixed_size.cpp
+++ STL/Vector/initialize_fixed_size.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
 
-   vector<int>v(3);  //// size = 3, all elements default-initialized
+   vector<int>v(3);  // size = 3, all elements default-initialized
 
    v[0] = 1; 
    v[1] = 2;
    v[2] = 0;
 
-   cout<<v.size()<<endl; ////print size 3
+   cout<<v.size()<<endl; //print size 3
 
    v.push_back(5);
 
