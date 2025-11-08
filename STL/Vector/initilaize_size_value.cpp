5c834aec62a8e55e2a98c820a6a1a049b2a13931
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-08 18:35:04 +0600

    Create initilaize_size_value.cpp

---
+++ STL/Vector/initilaize_size_value.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   vector<int>v(3, 5);  //size = 3, all elements initialized with value 5
+
+   cout<<v.size()<<endl; //print size 3
+   
+
+}
