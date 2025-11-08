859272243a8a41b367b8e8d9d15c2c936c331f95
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-08 18:40:20 +0600

    Create initialize_using_copy_constructor.cpp

---
+++ STL/Vector/initialize_using_copy_constructor.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   vector<int>v{1, 2, 3}; 
+
+   vector<int>v2(v) //copy vector v into v2;
+
+   vector<int>v3 = v; //both are same
+
+}
