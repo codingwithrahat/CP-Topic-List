277fe6ef27e5e924ecff532a5942a2124e86510a
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-08 18:41:48 +0600

    Update initialize_using_copy_constructor.cpp

--- STL/Vector/initialize_using_copy_constructor.cpp
+++ STL/Vector/initialize_using_copy_constructor.cpp
@@ -6,6 +6,7 @@ int main(){
    vector<int>v{1, 2, 3}; 
 
    vector<int>v2(v) //copy vector v into v2;
+   //copy constructor only work for same type (like vector to vector)
 
    vector<int>v3 = v; //both are same
 
