01ad80e93f931d72f6b8bb16b30296e2eb9c8055
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2025-11-08 18:23:34 +0600

    Create initialize_using_range_constructor.cpp

---
+++ STL/Vector/initialize_using_range_constructor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   set<int>s;
+
+   s.insert(1);
+   s.insert(2);
+   s.insert(3);
+
+   vector<int>v(s.begin(), s.end());
+
+   //Copies all elements from the set into a vector.
+   //TC - O(n), n is size of set
+
+}
