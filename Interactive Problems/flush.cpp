c0a9df9e06c491a1d66ae0d9221c7aa6da2ad88b
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-02-06 10:55:09 +0600

    Create flush.cpp

---
+++ Interactive Problems/flush.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define druto() ios::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+#define ff first
+#define ss second
+#define int long long
+#define double long double
+#define pb push_back
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define deci(x) cout << fixed << setprecision(x);
+
+const int INF = 1e18;
+const int mod = 1e9 + 7;
+
+
+int32_t main(){
+
+   druto();
+   int t; cin>>t;
+   while(t--){
+    int n; cin>>n;
+    cout<<n<<endl;
+    cout.flush();    //get output after every input
+   }
+
+
+}
