5a708244c519f53d2567560621efad06dc29339b
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-04-19 15:53:48 +0600

    Add example of vector pop_back usage
    Demonstrates the use of pop_back() on a vector.

---
+++ STL/Vector/04_pop_back.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   vector<int>v = {1, 2, 3};
+
+   v.pop_back(); //delete from last - 3
+   //O(1)
+
+   cout<<v.size()<<endl;  //size 2
+
+   
+   
+
+}
