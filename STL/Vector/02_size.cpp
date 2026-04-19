15da1218b5221df73d0806b851299632669ad745
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-04-19 15:52:12 +0600

    Implement vector size example in 02_size.cpp
    Add a program to demonstrate vector size retrieval

---
+++ STL/Vector/02_size.cpp
@@ -0,0 +1,17 @@
+//local vector max size 10^5
+//global vector max size 10^7
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   vector<int>v = {1, 2, 3, 4, 5};
+
+
+   cout<<v.size()<<endl;   //O(1)
+
+
+   
+
+}
